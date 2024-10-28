d8ad613cb7d637e5f5dca18ff990d38957370ffc
MohamedHassenBenadel <mohamedhassenbenadel@gmail.com>
2024-10-28 16:26:25 +0100

    bottom bar + responsive 28/10/2024

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 
@@ -9,7 +9,23 @@ import { Router } from '@angular/router';
 })
 export class SidebarComponent {
 
-  constructor(private router: Router) {}
+  isSidebarVisible: boolean = true; // Sidebar is visible by default
+  isMobile: boolean = false; // Initially set to false
+
+  // Listen for window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkViewportSize(); // Check viewport size on resize
+  }
+
+  constructor(private router: Router) {
+    this.checkViewportSize(); // Check viewport size on initialization
+  }
+
+  checkViewportSize() {
+    this.isMobile = window.innerWidth <= 768; // Set isMobile based on width
+    this.isSidebarVisible = !this.isMobile; // Hide sidebar on mobile
+  }
 
   isActiveRoute(route: string): boolean {
    // return this.router.url === route;
